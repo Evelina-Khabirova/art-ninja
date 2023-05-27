a2e90e9d373c49fc7f8638e32a7b744bb62f71dd
Khabirova Evelina <evelina.khabirova@yandex.ru>
2023-05-27 22:26:15 +0300

    Изменена анимация шапки и фона

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const header = document.querySelector('.header');
 const companyName = header.querySelector('.header__company-name');
 const emailText = header.querySelector('.header__email-text');
+const lead = document.querySelector('.lead');
 
 function changeHeader() {
-  if (window.scrollY > 50 && !header.classList.contains('header_white')) {
+  if (window.scrollY > 700 && !header.classList.contains('header_white')) {
     header.classList.add('header_white');
     companyName.classList.add('header__company-name_black');
     emailText.classList.add('header__email-text_black');
   }
-  else if (window.scrollY < 50 && header.classList.contains('header_white')) {
+  else if (window.scrollY < 700 && header.classList.contains('header_white')) {
     header.classList.remove('header_white');
     companyName.classList.remove('header__company-name_black');
     emailText.classList.remove('header__email-text_black');
@@ -18,4 +19,17 @@ function changeHeader() {
   }
 }
 
+function changeLead() {
+  if (window.scrollY > 700 && !lead.classList.contains('lead_dark')) {
+    lead.classList.add('lead_dark');
+  }
+  else if (window.scrollY < 700 && lead.classList.contains('lead_dark')) {
+    lead.classList.remove('lead_dark');
+  }
+  else {
+    return;
+  }
+}
+
+document.addEventListener('scroll', changeLead);
 document.addEventListener('scroll', changeHeader);
